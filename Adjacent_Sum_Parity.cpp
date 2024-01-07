8d724186ebed204d67312e64ebc9b5e8389a03cf
necro <acoustify69420@gmail.com>
2024-01-07 11:24:38 +0530

    updates

--- Adjacent_Sum_Parity.cpp
+++ Adjacent_Sum_Parity.cpp
@@ -5,7 +5,30 @@ int main()
  int T;
  cin>>T;
  while(T){
-    cout<<"Something"<<endl;
+    int n;
+    cin>>n;
+        int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cin>>arr[i];
+    }
+    int sum=0;
+    for (int i = 0; i <n; i++)
+    {
+        sum+=arr[i];
+    }
+    if (sum%2==0)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+        
+    
+    
     T--;
  }
 }
